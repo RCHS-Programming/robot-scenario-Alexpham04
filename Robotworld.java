9057faaa2104bb51e8d9a34581305f6e01787627
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-03-30 08:28:39 -0400

    added code to robot world and the block code

--- Robotworld.java
+++ Robotworld.java
@@ -3,19 +3,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class rivets here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Alex pham 
+ * @version 3-30-21
  */
-public class rivets extends World
+public class Robotworld extends World
 {
 
     /**
      * Constructor for objects of class rivets.
      * 
      */
-    public rivets()
+    public Robotworld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
     }
 }
