0a8e0cda03d2fd5ac19e2ca38983c594753519c5
Alexpham04 <Alexander.pham@stu.boone.kyschools.us>
2021-04-01 08:27:00 -0400

    added preset objects

--- Robotworld.java
+++ Robotworld.java
@@ -15,7 +15,22 @@ public class Robotworld extends World
      */
     public Robotworld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        
+      super(800, 600, 1); 
+      addObject(new Robot(),70,70);
+      addObject(new Wall(),50,150);
+      addObject(new Wall(),155,150);
+      addObject(new Block(),325,150);
+      addObject(new Wall(),500,150);
+      addObject(new Wall(),600,150);
+      addObject(new Wall(),700,150);
+      addObject(new Wall(),780,150); 
+      addObject(new Home(),750,550);
+      addObject(new Scorepanel(),80,550);
+      addObject(new Pizza(),120,70);
+      addObject(new Pizza(),250,200);
+      addObject(new Pizza(),250,280);
+      addObject(new Pizza(),375,445);
+      addObject(new Pizza(),650,550);
     }
 }
